e7ee49222115c2aa55c78b6bb6d04d00cff96049
count-infinity <count-infinity@easyday.dev>
2024-02-07 21:39:50 -0700

    Start dynamic programming solution

--- knapsack.py
+++ knapsack.py
@@ -51,6 +51,52 @@ def take_until_cap(items, capacity):
 
   return (value, taken)
 
+def dynamic(items, capacity):
+  dtable=[[0 for i in range(items)] for j in range(capacity+1)]
+
+
+  for idx, item in enumerate(items):
+    # Too heavy to ever take
+    if item.weight > capacity: continue
+
+    for weight in range(capacity+1):
+
+      # Item doesn't fit at this weight
+      if item.weight > weight:
+        dtable[weight][idx] = 0
+        continue
+      
+      # First item
+      if idx == 0:
+        dtable[weight][idx] = item.value
+        continue
+
+
+      prev_item_idx = idx-1
+      prev_idx_without_weight = max(weight-item.weight,0)
+
+      prev_best_value = dtable[weight][prev_item_idx]
+      prev_best_value_without_weight = dtable[prev_idx_without_weight][prev_item_idx]
+
+      best_value_if_chosen = prev_best_value_without_weight + item.value
+
+      dtable[weight][idx] = max(prev_best_value, best_value_if_chosen)
+
+
+
+
+
+
+
+      
+
+
+
+    
+
+
+
+
 
   
 
